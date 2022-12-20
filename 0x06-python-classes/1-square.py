017e0c0154a45a800a8be10c8bd1980477496f86
banelemncube <mncubebanele@gmail.com>
2022-12-20 05:04:17 -0800

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""create class square with private instance attribute size"""
+
+
+class Square:
+    """defines class and instantiates private attribute size."""
+    def __init__(self, size):
+        self.__size = size
