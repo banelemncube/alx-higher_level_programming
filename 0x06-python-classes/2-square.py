bea2effa821ca1d707b097525977099c2a0b51aa
banelemncube <mncubebanele@gmail.com>
2022-12-20 06:12:23 -0800

    tasks

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
+"""creates class Square with private instance attribute size"""
+
 
 class Square:
-
+    """defines class and
+instantiates private instance attribute size with validation"""
 
     def __init__(self, size=0)
+    """comment"""
+    
         if type(size) is not int:
             raise TypeError("Size must be an integer")
         elif size < 0:
