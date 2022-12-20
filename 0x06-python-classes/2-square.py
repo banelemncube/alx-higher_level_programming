60f97a74c8da8434c8fe9d47133af0cbc7d92b62
banelemncube <mncubebanele@gmail.com>
2022-12-20 06:19:43 -0800

    tasks

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -10,7 +10,7 @@ instantiates private instance attribute size with validation"""
     """comment"""
     
         if type(size) is not int:
-            raise TypeError("Size must be an integer")
+            raise TypeError('Size must be an integer')
         elif size < 0:
-            raise ValueError("Size must be >= 0")
+            raise ValueError('Size must be >= 0')
         self.__size = size
