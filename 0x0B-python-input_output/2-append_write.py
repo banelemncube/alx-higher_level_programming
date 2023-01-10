2d50e2a51e94732de667ce924cfb9d3da4348a98
banelemncube <mncubebanele@gmail.com>
2023-01-10 06:03:46 -0800

    taks

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""defines a function that appends a string at the end of a text file"""
+"""defines function to appends string to text file"""
 
 
 def append_write(filename="", text=""):
-"""appends string to text file"""
+    """appends string to text file"""
     with open(filename, 'a') as f:
         return f.write(text)
