3079db01c5cdc6dcbe5a6a4a4acfd9f7e95d9396
banelemncube <mncubebanele@gmail.com>
2023-01-10 05:27:44 -0800

    task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """defines function to read text file and prints to stdout"""
 
+
 def read_file(filename=""):
-"""reads text file and prints to stdout"""
-    with open("my_file_0.txt") as f
-          print(f.read(), end="")
+    """reads text file and prints to stdout"""
+    with open(filename) as f:
+        print(f.read(), end="")
